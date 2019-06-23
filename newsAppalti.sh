36dcb32203572b5164b1ef5c5cdfac8e3a993f93
Andrea Borruso <aborruso@gmail.com>
2019-06-23 19:32:39 +0200

    inizio

---
+++ newsAppalti.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+
+folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+mkdir -p "$folder"/dati
+
+# scarica dati di base
+curl "https://portaleappalti.comune.palermo.it/PortaleAppalti/it/ppgare_doc_news.wp" |
+  # pulisci l'HTML
+  tidy -q --show-warnings no --drop-proprietary-attributes y --show-errors 0 --force-output y |
+  # estrai le informazioni
+  scrape -be '//form/div[@class="list-item"]' | xq '.html.body.div[]|{data:.div[0]."#text",oggetto:.div[1]."#text",testo:.div[2]."#text",riferimento:.div[3]."#text",url:.div[5].a."@href"}' |
+  # rimuovi "a capo"
+  sed 's/\\n/ /g' |
+  # converti json in TSV
+  mlr --j2t cat >"$folder"/dati/news.tsc
