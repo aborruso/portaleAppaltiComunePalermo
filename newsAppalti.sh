b5a8c548e0147ac5332e1f6ad9ac417242dd573d
Andrea Borruso <aborruso@gmail.com>
2019-06-23 23:41:03 +0200

    aggiunto titolo

--- newsAppalti.sh
+++ newsAppalti.sh
@@ -11,7 +11,7 @@ curl "https://portaleappalti.comune.palermo.it/PortaleAppalti/it/ppgare_doc_news
   # pulisci l'HTML
   tidy -q --show-warnings no --drop-proprietary-attributes y --show-errors 0 --force-output y |
   # estrai le informazioni
-  scrape -be '//form/div[@class="list-item"]' | xq '.html.body.div[]|{data:.div[0]."#text",oggetto:.div[1]."#text",testo:.div[2]."#text",riferimento:.div[3]."#text",url:.div[5].a."@href"}' |
+  scrape -be '//form/div[@class="list-item"]' | xq '.html.body.div[]|{data:.div[0]."#text",oggetto:.div[1]."#text",testo:.div[2]."#text",riferimento:.div[3]."#text",titolo:.div[4]."#text",url:.div[5].a."@href"}' |
   # rimuovi "a capo"
   sed 's/\\n/ /g' |
   # converti json in TSV
